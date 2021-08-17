4fe6cf10e8e0e44278c5743478755d4877af83e0
MagnusStrom <magnuscock69@gmail.com>
2021-08-17 12:57:22 -0700

    Update for more numbers

--- cum.js
+++ cum.js
@@ -7,8 +7,18 @@ var num = 5;
 console.log("starting, getting number " + num);
 `;
 
+var i = 0;
+
+while (i < 100000) {
+    content += `
+    ${i == 0 ? "" : "else" } if (num == ${i}) {
+        console.log('number is ${i % 2 == 0 ? "even" : "odd"}');
+    }`
+    i++;
+  }
+
 console.log('writing');
-fs.writeFile('./test.js', content, err => {
+fs.writeFile('./index.js', content, err => {
   if (err) {
     console.error(err)
     return
--- test.js
+++
@@ -1,3 +0,0 @@
-
-var num = 5;
-console.log("starting, getting number " + num);
